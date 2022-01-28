fdcc2c9eb047509f32d79e0d8d54c585909c548e
sreekanth <sb9478420@gmail.com>
2022-01-28 22:59:04 +0530

    push

---
+++ src/main/java/com/sriman/app/App.java
@@ -0,0 +1,13 @@
+package com.sriman.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
